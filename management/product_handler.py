4dbe8892cf178dd242faccd9a4772326397db78e
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-05-25 08:50:44 -0300

    tab_handler file added and add_product and calculate_tab functions were included too

--- management/product_handler.py
+++ management/product_handler.py
@@ -20,3 +20,19 @@ def get_products_by_type(type):
             products_list.append(product)
 
     return products_list
+
+
+def add_product(menu: list, **kwargs):
+    new_product = kwargs
+
+    if len(menu) == 0:
+        new_product["_id"] = 1
+    else:
+        id_list = []
+        for product in menu:
+            id_list.append(product["_id"])
+        max_id = max(id_list)
+        new_product["_id"] = max_id + 1
+
+    menu.append(new_product)
+    return new_product
---
+++ management/tab_handler.py
@@ -0,0 +1,20 @@
+from menu import products
+import functools
+
+
+def calculate_tab(table: list):
+    payment_list = []
+    resulted_dict = {}
+
+    for product in products:
+        for item in table:
+            if item["_id"] == product["_id"]:
+                separate_payment = item["amount"] * product["price"]
+                payment_list.append(separate_payment)
+
+    completed_payment = functools.reduce(lambda a, b: a + b, payment_list)
+    resulted_dict["subtotal"] = f"${round(completed_payment, 2)}"
+    return resulted_dict
+
+
+calculate_tab([{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}])
